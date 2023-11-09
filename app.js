de1a6ed43ca952c56b419682086e04f6b591c66f
Malav Patel <patel.mala@northeastern.edu>
2023-11-09 18:25:23 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './Lab5.js';
+import cors from 'cors';
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
+import "dotenv/config.js";
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin:process.env.FRONTEND_URL
+}))
+app.use(express.json())
+Hello(app)
+Lab5(app)
+CourseRoutes(app)
+ModuleRoutes(app)
+AssignmentRoutes(app)
+app.listen(process.env.PORT ||4000)
---
+++ assignments/routes.js
@@ -0,0 +1,71 @@
+import Database from "../Database/index.js";
+function AssignmentRoutes(app) {
+  app.get("/api/assignments", (req, res) => {
+    const assignments = Database.assignments;
+    res.json(assignments);
+  });
+  app.get("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    const assignment = Database.assignments.find(
+      (assignment) => assignment._id === id
+    );
+    if (!assignment) {
+      res.status(404).send("Assignment not found");
+      return;
+    }
+    res.json(assignment);
+  });
+  app.get("/api/courses/:id/assignments", (req, res) => {
+    const { id } = req.params;
+    const assignments = Database.assignments.filter(
+      (assignment) => assignment.course === id
+    );
+    res.json(assignments);
+  });
+
+  app.delete("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    const index = Database.assignments.findIndex(
+      (assignment) => assignment._id === id
+    );
+    if (index === -1) {
+      res.status(404).send("Assignment not found");
+      return;
+    }
+    Database.assignments.splice(index, 1);
+    res.json(204);
+  });
+  app.post("/api/courses/:cid/assignments", (req, res) => {
+    const newAssignment = {
+      ...req.body,
+      course: req.params.cid,
+      _id: new Date().getTime().toString(),
+    };
+    Database.assignments.unshift(newAssignment);
+    res.json(newAssignment);
+  });
+  app.put("/api/assignments/:id", (req, res) => {
+    const { id } = req.params;
+    const index = Database.assignments.findIndex(
+      (assignment) => assignment._id === id
+    );
+    if (index === -1) {
+      res.status(404).send("Assignment not found");
+      return;
+    }
+    Database.assignments[index] = {
+      ...Database.assignments[index],
+      ...req.body,
+    };
+    res.json(200);
+  });
+
+    app.get("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const assignments = Database.assignments.filter(
+        (assignment) => assignment.course === cid
+        );
+        res.json(assignments);
+    });
+}
+export default AssignmentRoutes;
