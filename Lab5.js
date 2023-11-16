55791d9ecfde5c629bfa06065e149263c6d8f71c
Malav Patel <patel.mala@northeastern.edu>
2023-11-15 21:38:55 -0500

    Lab Done!

--- Lab5.js
+++ Lab5.js
@@ -7,7 +7,7 @@ const assignment = {
   score: 0,
 };
 const todos = [
-  { id: 1, title: "Task 1", completed: false },
+  { id: 1, title: "Task 1", completed: true },
   { id: 2, title: "Task 2", completed: false },
   { id: 3, title: "Task 3", completed: false },
   { id: 4, title: "Task 4", completed: false },
@@ -76,26 +76,42 @@ const Lab5 = (app) => {
       const { completed } = req.query;
       if (completed !== undefined) {
         const completedTodos = todos.filter(
-          (t) => t.completed === completed);
+          (t) => t.completed === true);
         res.json(completedTodos);
         return;
       }  
       res.json(todos);
     });
+
+
+   
+     app.get("/a5/todos/create", (req, res) => {
+      const newTodo = {
+        id: new Date().getTime(),
+        title: "New Task",
+        completed: false,
+      };
+      todos.push(newTodo);
+      res.json(todos);
+    });
+
     app.get("/a5/todos/:id", (req, res) => {
       const { id } = req.params;
       const todo = todos.find((todo) => todo.id === parseInt(id));
       res.json(todo);
     });
-    // app.get("/a5/todos/create", (req, res) => {
-    //   const newTodo = {
-    //     id: new Date().getTime(),
-    //     title: "New Task",
-    //     completed: false,
-    //   };
-    //   todos.push(newTodo);
-    //   res.json(todos);
-    // });
+
+   
+
+
+    app.get("/a5/todos/:id/delete", (req, res) => {
+      const { id } = req.params;
+      const todo = todos.find((t) => t.id === parseInt(id));
+      todos.splice(todos.indexOf(todo), 1);
+      res.json(todos);
+    });
+  
+
     app.post("/a5/todos", (req, res) => {
       const newTodo = {
         ...req.body,
